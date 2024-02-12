dffd0f9f4d5310a927567ea57f9b9691a027e03c
Rohit Kumar Gupta <104274011+rg-rohit9431@users.noreply.github.com>
2024-02-13 02:05:26 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import Resume from "../assets/Resume.pdf";
+import Resume from "../assets/latest resume.pdf";
 import { Button } from '@react-email/components';
 import { MdOutlineFileDownload } from "react-icons/md";
 import { MdDownloadDone } from "react-icons/md";
